1429852cbf3a928ef42573d03437fbe262e73dde
ducluu177@gmail.com <ducluu177@gmail.com>
2024-06-14 22:09:35 +0700

    Update list product

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -33,29 +33,32 @@ const ProductCard = ({ product, view }) => {
                 <img className={`${view === "list" ? 'w-full h-full object-cover group-hover:scale-110 duration-300' : 'w-full h-full object-cover group-hover:scale-110 duration-300'}`} src={product.url} alt="productImage" />
             </div>
             <div className="w-full border-[1px] px-2 py-4">
+                <div className="flex ml-[70px] ">
+                    <img alt="name" src={product.url} class="h-10 w-10 rounded-full object-contain" />
+                    <img alt="name" src={product.url} class="h-10 w-10 rounded-full object-contain" />
+                    <img alt="name" src={product.url} class="h-10 w-10 rounded-full object-contain" />
+                </div>
                 <div className={`${view === 'list' ? 'flex-col' : 'flex justify-between items-center'}`}>
                     <div className='w-full justify-center items-center'>
-                        <h2 className="text-base font-bold">{product?.name_product?.substring(0, 15)}</h2>
+                        <p className="text-base text-gray-600 text-center">{product?.name_product?.substring(0, 15)}</p>
                         {view === 'list' && (
                             <div className='w-2/4 mb-5'>
                                 <p className='text-sm text-gray-600'>{product.description}</p>
                             </div>
                         )}
                     </div>
-                    <div className={`${view === 'list' ? 'flex relative overflow-hidden w-full' : 'flex justify-end gap-2 relative overflow-hidden w-44 text-sm'}`}>
-                        <div className={`${view === 'list' ? 'flex-col transform group-hover:translate-x-24 transition-transform duration-500' : 'flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500'}`}>
-                            <p className="line-through text-gray-500">${product.price}</p>
-                            <p className="font-semibold">${product.price_sale}</p>
+                    
+                </div>
+                <div className={`${view === 'list' ? 'flex relative overflow-hidden w-full' : 'ml-[60px] flex justify-end gap-2 relative overflow-hidden w-[120px]'}`}>
+                        <div className={`${view === 'list' ? 'flex-col transform group-hover:translate-x-24 transition-transform duration-500' : 'font-bold flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500'}`}>
+                            <h2 className="line-through">${product.price}</h2>
+                            <h2 className="font-semibold">${product.price_sale}</h2>
                         </div>
-                        <p onClick={handleAddToCart} className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500 ">
+                        <p onClick={handleAddToCart} className=" mr-[15px] text-sm absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500 ">
                             Add to cart <ArrowForwardOutlinedIcon />
                         </p>
                     </div>
-                </div>
-                <div className={`${view === 'list' ? 'w-1/3 bottom-2 absolute' : ''}`}>
-                    <p className={`${view === 'list' ? 'text-base font-bold uppercase' : ''}`}>{product.name}</p>
-                </div>
-                {product.is_hot && (
+                {(product.is_hot != 0) && (
                     <div className={`${view === 'list' ? 'absolute top-4 left-0' : 'absolute top-4 right-0'}`}>
                         <p className='bg-red-600 text-white font-semibold uppercase px-4 py-1'>Sale</p>
                     </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -8,7 +8,7 @@ const Products = ({ products }) => {
                 <h1 className="text-4xl py-5 font-bold text-center">TRENDING THIS WEEK</h1>
             </div>
             <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-4 gap-10">
-                {products.slice(0, 4).map((item) => (
+                {products.slice(0, 8).map((item) => (
                     <ProductCard key={item._id} product={item} />
                 ))}
             </div>
